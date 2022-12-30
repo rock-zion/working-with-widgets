3a1f5d3b4b20cb18df0d7afc4ef885326c5599d4
rockxzion <rock_zion@outlook.com>
2022-12-30 18:09:06 +0100

    define string variables to display on the card

---
+++ lib/card1.dart
@@ -0,0 +1,6 @@
+  // 1
+  final String category = 'Editor\'s choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread';
+  final String chef = 'Ray Wenderlich';
+
