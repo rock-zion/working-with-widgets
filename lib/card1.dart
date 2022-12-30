d4918756125a1bcf5236ad848307ed0b50d28ce8
rockxzion <rock_zion@outlook.com>
2022-12-30 18:20:44 +0100

    start with a container in the center

--- lib/card1.dart
+++ lib/card1.dart
@@ -8,6 +8,9 @@ class Card1 extends StatelessWidget {
   final String description = 'Learn to make the perfect bread';
   final String chef = 'Ray Wenderlich';
 
+  // 2
+  @override
+  Widget build(BuildContext context) {
     // 3
     return Center(
         //  TODO: Card1 Decorate Container
