430f639acca28d69a5892fb2775f52676a0ee759
rockxzion <rock_zion@outlook.com>
2022-12-31 13:13:33 +0100

    Update card2.dart

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'author_card.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -13,13 +15,19 @@ class Card2 extends StatelessWidget {
               height: 450,
             ),
             decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/mag5.png'), fit: BoxFit.cover),
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              image: DecorationImage(
+                  image: AssetImage('assets/mag5.png'), fit: BoxFit.cover),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
             // 2
             child: Column(
               children: [
                 // TODO 1: add author information
+                const AuthorCard(
+                  authorName: 'Mike Katz',
+                  title: 'Smoothie Connoisseur',
+                  imageProvider: AssetImage('assets/author_katz.jpeg'),
+                ),
                 // TODO 4:  add Positioned text
                 // 1
                 Expanded(
