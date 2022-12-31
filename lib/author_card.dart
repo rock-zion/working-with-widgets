5adfd50ae58039ec07cd39e3d9d46d219d5fc2d4
rockxzion <rock_zion@outlook.com>
2022-12-31 07:37:08 +0100

    Create author_card.dart

---
+++ lib/author_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'circle_image.dart';
+
+class AuthorCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  }
+}
