65932ba460a18044e8d2a280f261de4849a1c4a4
rockxzion <rock_zion@outlook.com>
2022-12-31 07:38:02 +0100

    add three properties to author card:
    authorName
    title
    imageProvider

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -3,6 +3,18 @@ import 'fooderlich_theme.dart';
 import 'circle_image.dart';
 
 class AuthorCard extends StatelessWidget {
+  // 1
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard(
+      {Key? key,
+      required this.authorName,
+      required this.title,
+      this.imageProvider})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
   }
