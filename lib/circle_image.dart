7b6ff1b8f964e68921798ee65a0103bd87cb3782
rockxzion <rock_zion@outlook.com>
2022-12-31 07:22:36 +0100

    add parameters to circle image, imageProvider, imageRadius

--- lib/circle_image.dart
+++ lib/circle_image.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 class CircleImage extends StatelessWidget {
+  // 1
+  const CircleImage({
+    Key? key,
+    this.imageProvider,
+    this.imageRadius = 20,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
   }
